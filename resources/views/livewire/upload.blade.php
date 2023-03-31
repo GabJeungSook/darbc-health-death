e1192a88b5f263e95b27d87cafbc65d50c7b0f41
GabJeungSook <gicawalo@gmail.com>
2023-03-31 09:55:26 +0800

    upload and display death

--- resources/views/livewire/upload.blade.php
+++ resources/views/livewire/upload.blade.php
@@ -9,6 +9,13 @@
   <div class="mt-1">
     <div class="flex flex-col border p-2 space-y-1 mb-1">
       <label for="member">Health death</label>
+      <input type="file" wire:model="health">
+    </div>
+    <x-button label="Upload" wire:click="uploadHealth" spinner="health" dark />
+  </div>
+  <div class="mt-1">
+    <div class="flex flex-col border p-2 space-y-1 mb-1">
+      <label for="member">Death</label>
       <input type="file" wire:model="death">
     </div>
     <x-button label="Upload" wire:click="uploadDeath" spinner="death" dark />
