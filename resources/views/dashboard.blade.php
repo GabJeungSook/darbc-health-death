a38ca4307568716c67bebc2c9eb466fad0de6f01
GabJeungSook <gicawalo@gmail.com>
2023-05-08 13:34:25 +0800

    changed dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -10,24 +10,25 @@
           <p class="mt-2 text-3xl font-bold tracking-tight text-gray-700 sm:text-4xl">Health & Death Management</p>
 
           <dl class="mt-16 grid max-w-xl grid-cols-1 gap-8 sm:mt-20 sm:grid-cols-2 xl:mt-16">
-            <div class="flex flex-col gap-y-3 border-l border-gray-900/10 pl-6">
-              <dt class="text-sm leading-6 text-gray-600">Creators on the platform</dt>
-              <dd class="order-first text-3xl font-semibold tracking-tight text-gray-900">8,000+</dd>
+            <div class="flex flex-col gap-y-3 border-l border-white pl-6">
+              {{-- <dt class="text-sm leading-6 text-gray-600">Creators on the platform</dt>
+              <dd class="order-first text-3xl font-semibold tracking-tight text-gray-900">8,000+</dd> --}}
             </div>
 
-            <div class="flex flex-col gap-y-3 border-l border-gray-900/10 pl-6">
-              <dt class="text-sm leading-6 text-gray-600">Flat platform fee</dt>
-              <dd class="order-first text-3xl font-semibold tracking-tight text-gray-900">3%</dd>
+            <div class="flex flex-col gap-y-3 border-l border-white pl-6">
+              {{-- <dt class="text-sm leading-6 text-gray-600">Flat platform fee</dt>
+              <dd class="order-first text-3xl font-semibold tracking-tight text-gray-900">3%</dd> --}}
             </div>
 
-            <div class="flex flex-col gap-y-3 border-l border-gray-900/10 pl-6">
-              <dt class="text-sm leading-6 text-gray-600">Uptime guarantee</dt>
-              <dd class="order-first text-3xl font-semibold tracking-tight text-gray-900">99.9%</dd>
+            <div class="flex flex-col gap-y-3 border-l border-white pl-6">
+              {{-- <dt class="text-sm leading-6 text-gray-600">Uptime guarantee</dt>
+              <dd class="order-first text-3xl font-semibold tracking-tight text-gray-900">99.9%</dd> --}}
             </div>
 
-            <div class="flex flex-col gap-y-3 border-l border-gray-900/10 pl-6">
-              <dt class="text-sm leading-6 text-gray-600">Paid out to creators</dt>
-              <dd class="order-first text-3xl font-semibold tracking-tight text-gray-900">$70M</dd>
+            <div class="flex flex-col gap-y-3 pl-6">
+               {{--  <div class="flex flex-col gap-y-3 border-l border-gray-900/10 pl-6">
+               <dt class="text-sm leading-6 text-gray-600">Paid out to creators</dt>
+              <dd class="order-first text-3xl font-semibold tracking-tight text-gray-900">$70M</dd> --}}
             </div>
           </dl>
         </div>
