bbcc6e5287397d6807796a1df9a88521174c73a9
GabJeungSook <gicawalo@gmail.com>
2023-06-23 11:19:59 +0800

    revert kernel

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -47,7 +47,7 @@ class Kernel extends HttpKernel
         'api' => [
             // \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
             // 'throttle:500,1',
-            // \Illuminate\Routing\Middleware\ThrottleRequests::class.':api',
+            \Illuminate\Routing\Middleware\ThrottleRequests::class.':api',
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
         ],
     ];
