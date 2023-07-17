7bc6d0632d125771eff5846f42f60e2b38c2d6a7
GabJeungSook <gicawalo@gmail.com>
2023-07-17 16:06:12 +0800

    added darbc id to export

--- resources/views/exports/masterlist.blade.php
+++ resources/views/exports/masterlist.blade.php
@@ -3,6 +3,7 @@
         <thead class="font-normal">
               <tr>
                  <th class="border text-left whitespace-nowrap px-2 text-sm font-medium text-gray-500 py-2">ENROLLMENT STATUS</th>
+                 <th class="border text-left whitespace-nowrap px-2 text-sm font-medium text-gray-500 py-2">DARBC ID</th>
                   <th class="border text-left whitespace-nowrap px-2 text-sm font-medium text-gray-500 py-2">MEMBER NAME</th>
                   <th class="border text-left whitespace-nowrap px-2 text-sm font-medium text-gray-500 py-2">DEPENDENT NAME</th>
                   <th class="border text-left whitespace-nowrap px-2 text-sm font-medium text-gray-500 py-2">PURPOSE</th>
@@ -30,6 +31,8 @@
                   @endphp
                <td class="border text-gray-600  px-3 whitespace-nowrap py-1">{{ $item->enrollment_status }}
                </td>
+               <td class="border text-gray-600  px-3 whitespace-nowrap py-1">{{ $darbc_id }}
+                </td>
               <td class="border text-gray-600  px-3 whitespace-nowrap py-1">{{  $member_name }}
               </td>
               <td class="border text-gray-600  px-3 whitespace-nowrap py-1">{{ strtoupper($item->last_name).', '.strtoupper($item->first_name).' '.strtoupper($item->middle_name) }}
