d3ea2825497378a64a87cdd21645be6650df7350
GabJeungSook <gicawalo@gmail.com>
2023-09-15 11:30:21 +0800

    test encoded date

--- app/Http/Livewire/Report.php
+++ app/Http/Livewire/Report.php
@@ -41,9 +41,7 @@ class Report extends Component
             });
         })
         ->when($this->encoded_date, function ($query) {
-            $query->where(function ($query) {
-                $query->where('created_at', $this->encoded_date);
-            });
+            $query->where('created_at', $this->encoded_date);
         })
         ->when(!empty($this->status), function ($query) {
             if (is_array($this->status)) {
