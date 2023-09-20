122f1167ca17e0d27a298b7c3e6d7108e9cced84
GabJeungSook <gicawalo@gmail.com>
2023-09-20 09:05:55 +0800

    last name

--- resources/views/reports/health.blade.php
+++ resources/views/reports/health.blade.php
@@ -49,9 +49,9 @@
 
                 $collection = collect($member_data['data']);
                 $darbc_id = $collection['darbc_id'];
-                $member_name = strtoupper($collection['user']['surname']) . ' '
+                $member_name = strtoupper($collection['user']['surname']) . ', '
                 .strtoupper($collection['user']['first_name']) . ' '
-                . strtoupper($collection['user']['middle_name']).'.';
+                . strtoupper($collection['user']['middle_name']);
                 @endphp
             <td class="border text-gray-600  px-3 whitespace-nowrap py-1">{{ $member_name }}
             </td>
