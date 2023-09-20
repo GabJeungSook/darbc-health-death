5c9b1ecdab3c150a5d63842d7fea593a539bb491
GabJeungSook <gicawalo@gmail.com>
2023-09-20 09:52:46 +0800

    try again

--- app/Http/Livewire/Mortuary.php
+++ app/Http/Livewire/Mortuary.php
@@ -215,7 +215,7 @@ class Mortuary extends Component implements Tables\Contracts\HasTable
                 // return strtoupper($record->claimants_last_name) . ', ' . strtoupper($record->claimants_first_name) . ' ' . strtoupper($record->claimants_middle_name) ;
             })
             ->sortable()
-            ->searchable(query: function (Builder $query, string $search, $record): Builder {
+            ->searchable(query: function (Builder $query, string $search, MortuaryModel $record): Builder {
                 $url = 'https://darbcmembership.org/api/member-information/'.$record->member_id;
                 $response = Http::withOptions(['verify' => false])->get($url);
                 $member_data = $response->json();
