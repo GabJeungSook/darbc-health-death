eb6df74a7cd419c7c2fb4ea4d3071c2ef51935d8
GabJeungSook <gicawalo@gmail.com>
2023-11-17 13:24:57 +0800

    3xs mortuary

--- resources/views/reports/mortuary.blade.php
+++ resources/views/reports/mortuary.blade.php
@@ -15,19 +15,19 @@
         <thead class="font-normal">
           <tr>
               <tr>
-                  <th class="border text-left whitespace-nowrap px-2 text-sm font-medium text-gray-500 py-2">DATE</th>
-                  <th class="border text-left whitespace-nowrap px-2 text-sm font-medium text-gray-500 py-2">MEMBER NAME</th>
-                  <th class="border text-left whitespace-nowrap px-2 text-sm font-medium text-gray-500 py-2">DATE OF DEATH</th>
-                  <th class="border text-left whitespace-nowrap px-2 text-sm font-medium text-gray-500 py-2">CONTACT NUMBER</th>
-                  <th class="border text-left whitespace-nowrap px-2 text-sm font-medium text-gray-500 py-2">AMOUNT
+                  <th class="border text-left whitespace-nowrap px-2 text-2xs font-medium text-gray-500 py-2">DATE</th>
+                  <th class="border text-left whitespace-nowrap px-2 text-2xs font-medium text-gray-500 py-2">MEMBER NAME</th>
+                  <th class="border text-left whitespace-nowrap px-2 text-2xs font-medium text-gray-500 py-2">DATE OF DEATH</th>
+                  <th class="border text-left whitespace-nowrap px-2 text-2xs font-medium text-gray-500 py-2">CONTACT NUMBER</th>
+                  <th class="border text-left whitespace-nowrap px-2 text-2xs font-medium text-gray-500 py-2">AMOUNT
                   </th>
-                  <th class="border text-left whitespace-nowrap px-2 text-sm font-medium text-gray-500 py-2">CLAIMANT
+                  <th class="border text-left whitespace-nowrap px-2 text-2xs font-medium text-gray-500 py-2">CLAIMANT
                   </th>
-                  <th class="border text-left whitespace-nowrap px-2 text-sm font-medium text-gray-500 py-2">CLAIMANTS CONTACT NUMBER</th>
-                  <th class="border text-left whitespace-nowrap px-2 text-sm font-medium text-gray-500 py-2">STATUS</th>
-                  <th class="border text-left whitespace-nowrap px-2 text-sm font-medium text-gray-500 py-2">DIAMOND PACKAGE</th>
-                  <th class="border text-left whitespace-nowrap px-2 text-sm font-medium text-gray-500 py-2">VEHICLE</th>
-                  <th class="border text-left whitespace-nowrap px-2 text-sm font-medium text-gray-500 py-2">COVERAGE TYPE</th>
+                  <th class="border text-left whitespace-nowrap px-2 text-2xs font-medium text-gray-500 py-2">CLAIMANTS CONTACT NUMBER</th>
+                  <th class="border text-left whitespace-nowrap px-2 text-2xs font-medium text-gray-500 py-2">STATUS</th>
+                  <th class="border text-left whitespace-nowrap px-2 text-2xs font-medium text-gray-500 py-2">DIAMOND PACKAGE</th>
+                  <th class="border text-left whitespace-nowrap px-2 text-2xs font-medium text-gray-500 py-2">VEHICLE</th>
+                  <th class="border text-left whitespace-nowrap px-2 text-2xs font-medium text-gray-500 py-2">COVERAGE TYPE</th>
                 </tr>
           </tr>
         </thead>
@@ -45,18 +45,18 @@
                   .strtoupper($collection['user']['first_name']) . ' '
                   . strtoupper($collection['user']['middle_name']);
                   @endphp
-              <td class="border text-gray-600  px-3 whitespace-nowrap py-1">{{  \Carbon\Carbon::parse($item->created_at)->format('F, d Y') }}</td>
-              <td class="border text-gray-600  px-3 whitespace-nowrap py-1">{{  $member_name }}</td>
-              <td class="border text-gray-600  px-3 whitespace-nowrap py-1">{{  \Carbon\Carbon::parse($item->date_of_death)->format('F, d Y') }}</td>
-              <td class="border text-gray-600  px-3 whitespace-nowrap py-1">{{ $item->contact_number }}</td>
-             <td class="border text-gray-600  px-3 whitespace-nowrap py-1">{{ number_format($item->amount, 2) }}</td>
-             <td class="border text-gray-600  px-3 whitespace-nowrap py-1">{{ strtoupper($item->claimants_last_name). ', '.strtoupper($item->claimants_first_name).' '
+              <td class="border text-gray-600  text-3xs px-3 whitespace-nowrap py-1">{{  \Carbon\Carbon::parse($item->created_at)->format('F, d Y') }}</td>
+              <td class="border text-gray-600  text-3xs px-3 whitespace-nowrap py-1">{{  $member_name }}</td>
+              <td class="border text-gray-600  text-3xs px-3 whitespace-nowrap py-1">{{  \Carbon\Carbon::parse($item->date_of_death)->format('F, d Y') }}</td>
+              <td class="border text-gray-600  text-3xs px-3 whitespace-nowrap py-1">{{ $item->contact_number }}</td>
+             <td class="border text-gray-600  text-3xs px-3 whitespace-nowrap py-1">{{ number_format($item->amount, 2) }}</td>
+             <td class="border text-gray-600  text-3xs px-3 whitespace-nowrap py-1">{{ strtoupper($item->claimants_last_name). ', '.strtoupper($item->claimants_first_name).' '
              .strtoupper($item->claimants_middle_name) }}</td>
-             <td class="border text-gray-600  px-3 py-1 whitespace-pre-wrap">{{ $item->claimants_contact_number }}</td>
-             <td class="border text-gray-600  px-3 py-1 whitespace-pre-wrap">{{ $item->status }}</td>
-             <td class="border text-gray-600  px-3 py-1 whitespace-pre-wrap">{{ $item->diamond_package }}</td>
-             <td class="border text-gray-600  px-3 py-1 whitespace-pre-wrap">{{ $item->vehicle == 1 ? 'Yes' : 'No' }}</td>
-             <td class="border text-gray-600  px-3 py-1 whitespace-pre-wrap">
+             <td class="border text-gray-600  text-3xs px-3 py-1 whitespace-pre-wrap">{{ $item->claimants_contact_number }}</td>
+             <td class="border text-gray-600  text-3xs px-3 py-1 whitespace-pre-wrap">{{ $item->status }}</td>
+             <td class="border text-gray-600  text-3xs px-3 py-1 whitespace-pre-wrap">{{ $item->diamond_package }}</td>
+             <td class="border text-gray-600  text-3xs px-3 py-1 whitespace-pre-wrap">{{ $item->vehicle == 1 ? 'Yes' : 'No' }}</td>
+             <td class="border text-gray-600  text-3xs px-3 py-1 whitespace-pre-wrap">
                 @switch($item->coverage_type)
                 @case(1)
                     Accidental Death/ Disablement
