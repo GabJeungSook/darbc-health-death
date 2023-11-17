534c35eaed01e72f1ef15854a246c36f84cc2535
GabJeungSook <gicawalo@gmail.com>
2023-11-17 11:47:25 +0800

    removed dependent in report

--- resources/views/reports/belowten.blade.php
+++ resources/views/reports/belowten.blade.php
@@ -19,8 +19,8 @@
             </th>
             <th class="border text-left whitespace-nowrap px-2 text-sm font-medium text-gray-500 py-2">MEMBER NAME
             </th>
-            <th class="border text-left whitespace-nowrap px-2 text-sm font-medium text-gray-500 py-2">DEPENDENT NAME
-            </th>
+            {{-- <th class="border text-left whitespace-nowrap px-2 text-sm font-medium text-gray-500 py-2">DEPENDENT NAME
+            </th> --}}
             <th class="border text-left whitespace-nowrap px-2 text-sm font-medium text-gray-500 py-2">DATE OF CONFINEMENT FROM
             </th>
             <th class="border text-left whitespace-nowrap px-2 text-sm font-medium text-gray-500 py-2">DATE OF CONFINEMENT TO
@@ -48,26 +48,13 @@
                     <td class="border text-gray-600  px-3  py-1">{{$count++}}</td>
                     <td class="border text-gray-600 whitespace-nowrap  px-3  py-1">{{ strtoupper($collection['user']['surname']).', '.
                     strtoupper($collection['user']['first_name']).' '.strtoupper($collection['user']['first_name']) }}</td>
-                    @if ($item->enrollment_status == 'member')
-                    {{-- <td class="border text-gray-600 whitespace-nowrap  px-3  py-1">{{ strtoupper($collection['user']['first_name']).' '.
-                    strtoupper($collection['user']['middle_name']).' '.strtoupper($collection['user']['surname']) }}</td> --}}
+                    {{-- @if ($item->enrollment_status == 'member')
                     <td class="border text-gray-600  px-3 whitespace-nowrap py-1">---</td>
                     @else
                     <td class="border text-gray-600 whitespace-nowrap  px-3  py-1">{{ strtoupper($item->first_name).' '.
                     strtoupper($item->middle_name).'. '.strtoupper($item->last_name) }}</td>
-                    @endif
-              {{-- <td class="border text-gray-600  px-3  py-1">{{$count++}}</td> --}}
-              {{-- <td class="border text-gray-600 whitespace-nowrap  px-3  py-1">{{ strtoupper($collection['user']['first_name']).' '.
-              strtoupper($collection['user']['middle_name']).' '.strtoupper($collection['user']['surname']) }}</td> --}}
-               {{-- <td class="border text-gray-600 whitespace-nowrap  px-3  py-1">{{ strtoupper($item->first_name).' '.
-                strtoupper($item->middle_name).'. '.strtoupper($item->last_name) }}</td> --}}
-              {{-- @if ($item->enrollment_status == 'member') --}}
-              {{-- <td class="border text-gray-600 whitespace-nowrap  px-3  py-1">{{ strtoupper($collection['user']['first_name']).' '.
-                strtoupper($collection['user']['middle_name']).' '.strtoupper($collection['user']['surname']) }}</td> --}}
-              {{-- @else --}}
-              {{-- <td class="border text-gray-600 whitespace-nowrap  px-3  py-1">{{ strtoupper($item->first_name).' '.
-                strtoupper($item->middle_name).'. '.strtoupper($item->last_name) }}</td> --}}
-              {{-- @endif --}}
+                    @endif --}}
+
 
               <td class="border text-gray-600  px-3  py-1">{{ Carbon\Carbon::parse($item->confinement_date_from)->format('F d, Y') }}</td>
               <td class="border text-gray-600  px-3  py-1">{{ Carbon\Carbon::parse($item->confinement_date_to)->format('F d, Y') }}</td>
