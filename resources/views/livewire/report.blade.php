43d9e8eecba63721abc3c7bed529241bea2ebb8d
Gabriel Jon Icawalo <67619600+GabJeungSook@users.noreply.github.com>
2024-07-04 08:45:02 +0800

    test

--- resources/views/livewire/report.blade.php
+++ resources/views/livewire/report.blade.php
@@ -62,7 +62,7 @@
                 <x-select.option label="Dependent" value="dependent" />
             </x-select>
           </div>
-      @elseif ($report_get == 7 || $report_get == 29)
+      @elseif ($report_get == 7 ||  $report_get == 8 || $report_get == 29)
       <div class="flex space-x-2">
         <x-datetime-picker label="Encoded Date From" placeholder="Select Date" without-time without-timezone wire:model="encoded_date_from" />
         <x-datetime-picker label="Encoded Date To" placeholder="Select Date" without-time without-timezone wire:model="encoded_date_to" />
@@ -74,10 +74,13 @@
               <x-select.option label="Paid" value="PAID" />
               <x-select.option label="Unpaid" value="UNPAID" />
           </x-select>
+          @if ($report_get != 8)
           <x-select label="Enrollment Status" multiselect placeholder="All" wire:model="enrollment_status">
             <x-select.option label="Member" value="member" />
             <x-select.option label="Dependent" value="dependent" />
-        </x-select>
+          </x-select>
+          @endif
+          
       </div>
       @endif
     </div>
