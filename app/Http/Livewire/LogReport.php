e021effd037d7771419169a56da7cf86704e4ec1
Gabriel Jon Icawalo <67619600+GabJeungSook@users.noreply.github.com>
2025-02-24 15:00:17 +0800

    try again

--- app/Http/Livewire/LogReport.php
+++ app/Http/Livewire/LogReport.php
@@ -31,18 +31,18 @@ class LogReport extends Component
 
     public function render()
     {
-        // $this->logs = Log::when($this->date_from && $this->date_to, function ($query) {
-        //     $query->where(function ($query) {
-        //     $query->whereBetween('date_received', [$this->date_from, $this->date_to]);
-        //     });
-        // })
-        // ->when($this->encoded_date, function ($query) {
-        //     $query->whereDate('created_at', $this->encoded_date);
-        // })
-        // ->when($this->enrollment_status_selected, function ($query) {
-        //     $query->where('enrollment_status', $this->enrollment_status_selected);
-        // })
-        // ->paginate(100);
+        $this->logs = Log::when($this->date_from && $this->date_to, function ($query) {
+            $query->where(function ($query) {
+            $query->whereBetween('date_received', [$this->date_from, $this->date_to]);
+            });
+        })
+        ->when($this->encoded_date, function ($query) {
+            $query->whereDate('created_at', $this->encoded_date);
+        })
+        ->when($this->enrollment_status_selected, function ($query) {
+            $query->where('enrollment_status', $this->enrollment_status_selected);
+        })
+        ->paginate(100);
         return view('livewire.log-report',[
         'logs' => $this->report_get != 1 ? [] : ($this->logs == null ? [] : Log::when($this->date_from && $this->date_to, function ($query) {
             $query->where(function ($query) {
