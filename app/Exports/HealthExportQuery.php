e2678f210495d3c340537a9c41b18f01662af2b5
Gabriel Jon Icawalo <67619600+GabJeungSook@users.noreply.github.com>
2025-05-06 08:57:47 +0800

    ss

--- app/Exports/HealthExportQuery.php
+++ app/Exports/HealthExportQuery.php
@@ -3,15 +3,18 @@
 namespace App\Exports;
 
 use App\Models\Health;
-use Illuminate\Support\Facades\DB;
-use Maatwebsite\Excel\Concerns\FromQuery;
+use Illuminate\Support\Collection;
+use Maatwebsite\Excel\Concerns\FromCollection;
 use Maatwebsite\Excel\Concerns\WithHeadings;
+use Maatwebsite\Excel\Concerns\WithMapping;
+use Maatwebsite\Excel\Concerns\WithChunkReading;
 use Maatwebsite\Excel\Concerns\ShouldAutoSize;
 
-class HealthExportQuery implements FromQuery, WithHeadings, ShouldAutoSize
+
+class HealthExportQuery implements FromCollection, WithHeadings, WithMapping, WithChunkReading, ShouldAutoSize
 {
     protected $encoded_date_from, $encoded_date_to, $date_from, $date_to, $status, $enrollment_status;
-    /**
+        /**
     * @return \Illuminate\Support\Collection
     */
 
@@ -25,10 +28,9 @@ class HealthExportQuery implements FromQuery, WithHeadings, ShouldAutoSize
         $this->enrollment_status = $enrollment_status;
     }
 
-    public function query()
+    public function collection(): Collection
     {
-        return Health::query()
-            ->when($this->date_from && $this->date_to, function ($query) {
+        return Health::when($this->date_from && $this->date_to, function ($query) {
                 if ($this->date_from === $this->date_to) {
                     $query->where('confinement_date_from', $this->date_from);
                 } else {
@@ -43,11 +45,15 @@ class HealthExportQuery implements FromQuery, WithHeadings, ShouldAutoSize
                     $query->whereBetween(DB::raw('DATE(created_at)'), [$this->encoded_date_from, $this->encoded_date_to]);
                 }
             })
-            ->when(!empty($this->status), fn ($query) => is_array($this->status)
-                ? $query->whereIn('status', $this->status)
-                : $query->where('status', $this->status))
+            ->when(!empty($this->status), fn ($query) =>
+                is_array($this->status)
+                    ? $query->whereIn('status', $this->status)
+                    : $query->where('status', $this->status)
+            )
             ->when($this->enrollment_status, fn ($query) =>
-                $query->where('enrollment_status', $this->enrollment_status));
+                $query->where('enrollment_status', $this->enrollment_status)
+            )
+            ->get();
     }
 
     public function headings(): array
@@ -67,6 +73,32 @@ class HealthExportQuery implements FromQuery, WithHeadings, ShouldAutoSize
             'Amount',
             'Status',
         ];
+    }
+
+    public function map($item): array
+    {
+        return [
+            \Carbon\Carbon::parse($item->created_at)->format('F d, Y'),
+            $item->batch_number,
+            strtoupper($item->enrollment_status),
+            $item->darbc_id,
+            $item->member_name,
+            $item->enrollment_status === "member"
+                ? "---"
+                : trim("{$item->last_name} {$item->first_name} {$item->middle_name}"),
+            $item->age,
+            \Carbon\Carbon::parse($item->confinement_date_from)->format('F d, Y'),
+            \Carbon\Carbon::parse($item->confinement_date_to)->format('F d, Y'),
+            optional($item->hospitals)->name,
+            $item->number_of_days,
+            $item->amount,
+            $item->status,
+        ];
+    }
+
+    public function chunkSize(): int
+    {
+        return 1000; // adjust based on your memory capacity
     }
 
 }
